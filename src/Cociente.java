e9601dee117a222a6a2bf083db928a5901fe0ee4
Miguel <vilanovamiguel@hotmail.com>
2021-02-18 00:43:01 +0100

    Añado Comentarios a la clase Cociente

--- src/Cociente.java
+++ src/Cociente.java
@@ -1,4 +1,13 @@
+/**
+ * Clase para realizar operaciones de cocientes. (Los resultados que den números reales periódicos solo devolverán 16 decimales.)
+ * 
+ * @author Equipocucudrulu
+ * @since 24/01/2021
+ * @version 1.0
+ * 
+ */
 
 public class Cociente {
 
+	
 }

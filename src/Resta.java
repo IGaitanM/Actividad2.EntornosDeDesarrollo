1b72b5baf6dc2b7ee4da97d924812408c97a764c
antoprog <anto.programacion@gmail.com>
2021-02-19 07:37:02 +0100

    Resta dos numeros reales

--- src/Resta.java
+++ src/Resta.java
@@ -1,4 +1,26 @@
 
+/**
+ * 
+ * @author AntonioPastor // Equipo cucudrulu
+ * @since 24/01/2021
+ * @version 1.0
+ * 
+ */
+
 public class Resta {
+	public static int acumulado = 0;
+
+
+	/**
+	 * 1.  Método @restaDosReales para restar dos numeros reales
+	 * @param numReal1 generamos un numero real
+	 * @param numReal2 generamos un numero real
+	 * @return nos devuelve la resta de los numeros reales
+	 */
+		public double restaDosReales(double numReal1, double numReal2) {
+		
+		double resultadoresta = numReal1 - numReal2;
+				return resultadoresta;
+		}
 
 }

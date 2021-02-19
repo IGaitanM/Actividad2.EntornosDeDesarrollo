bd7fcf917d063be078427fcb2e734c16761a33e3
IGaitanM <ivanovef@gmail.com>
2021-02-19 18:17:21 +0100

    Método para multiplicar 2 números reales

--- src/Producto.java
+++ src/Producto.java
@@ -1,4 +1,25 @@
-
+/**
+ * Clase de la calculadora para realizar operaciones de multiplicación.
+ * 
+ * @author Iván Gaitán Muñoz
+ * @since 24/01/2021
+ * @version 1.0
+ * 
+ */
 public class Producto {
 
-}
+	/**
+	 * Método para multiplicar dos números reales.
+	 * 
+	 * @param numReal1 primer valor real para hacer la operación.
+	 * @param numReal2 segundo valor real para hacer la operación.
+	 * @return resultado devuelve la multiplicacion de los dos valores reales.
+	 */
+
+	public double multiplicacionDosReales(double numReal1, double numReal2) {
+
+		double resultado = numReal1 * numReal2;
+		return resultado;
+	}
+	
+}	

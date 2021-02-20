47645c30aa26118724a4e3e6aece6b4cb82fbbaa
Miguel <vilanovamiguel@hotmail.com>
2021-02-20 10:43:10 +0100

    Añado método para dividir dos números reales

--- src/Cociente.java
+++ src/Cociente.java
@@ -1,13 +1,25 @@
 /**
  * Clase para realizar operaciones de cocientes. (Los resultados que den números reales periódicos solo devolverán 16 decimales.)
  * 
- * @author Equipocucudrulu
+ * @author Miguel Vilanova - Equipocucudrulu
  * @since 24/01/2021
  * @version 1.0
  * 
  */
 
 public class Cociente {
+	/**
+	 * Método para dividir dos números reales.
+	 * 
+	 * @param numReal1 primer valor real para hacer la operación.
+	 * @param numReal2 segundo valor real para hacer la operación que sea diferente a 0.
 
+	 * @return resultado devuelve la división de los dos valores reales. 
+	 */
+	public double dividirDosReales(double numReal1, double numReal2) {
+
+	double resultado = numReal1 / numReal2;
+			return resultado ;
+	}
 	
 }

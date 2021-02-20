faca0653268e165210dae255efd966645c2342cc
Carlos Ashley <hdpspain@gmail.com>
2021-02-20 11:29:57 +0100

    Añaido prueba de metodo sumaDosReales a main

--- src/PruebaMetodos.java
+++ src/PruebaMetodos.java
@@ -7,6 +7,10 @@ public class PruebaMetodos {
 		Producto pro= new Producto ();
 		System.out.println(pro.multiplicacionTresReales(1.5556, 2.668, 4.5565));
 		
+		// Prueba método Suma. Carlos Ashley
+		Suma suma1 = new Suma();
+		System.out.println(suma1.sumaDosReales(5, 9));
+		
 	}
 
 }
